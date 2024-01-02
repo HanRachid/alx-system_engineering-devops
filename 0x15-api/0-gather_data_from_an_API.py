8f7ded9cb3ead384c68f9807f5ff3547f6ea877b
HanRachid <handaouirachid@gmail.com>
2024-01-02 13:01:52 +0100

    add documentation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
 """
-gather data from api and structure and print it 
+gather data from api and structure and print it
 """
 import sys
 
 import requests
 
-if (len(sys.argv) == 2):
-
+if len(sys.argv) == 2:
     ruser = requests.get(
-        'https://jsonplaceholder.typicode.com/users/'+sys.argv[1])
+        "https://jsonplaceholder.typicode.com/users/" + sys.argv[1] + "/"
+    )
     userresponse = ruser.json()
     rtodo = requests.get(
-        'https://jsonplaceholder.typicode.com/users/'+sys.argv[1]+'/todos')
-    EMPLOYEE_NAME = userresponse['name']
+        "https://jsonplaceholder.typicode.com/users/" + sys.argv[1] + "/todos"
+    )
+    EMPLOYEE_NAME = userresponse["name"]
     todoresponse = rtodo.json()
     NUMBER_OF_DONE_TASKS = 0
     TOTAL_NUMBER_OF_TASKS = 0
-    DONE_TASKS = ''
+    DONE_TASKS = ""
     for task in todoresponse:
         TOTAL_NUMBER_OF_TASKS += 1
-        if task['completed'] == True:
+        if task["completed"]:
             NUMBER_OF_DONE_TASKS += 1
-            DONE_TASKS += '\t '+task['title']+'\n'
+            DONE_TASKS += "\t " + task["title"] + "\n"
     print(
-        f'Employee {EMPLOYEE_NAME} is done with tasks ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
+        "Employee "
+        + str(EMPLOYEE_NAME)
+        + "is done with tasks ("
+        + str(NUMBER_OF_DONE_TASKS)
+        + "/"
+        + str(TOTAL_NUMBER_OF_TASKS)
+        + "):"
+    )
     print(DONE_TASKS)
