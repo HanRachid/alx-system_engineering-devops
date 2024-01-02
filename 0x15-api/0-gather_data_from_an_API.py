462e7b4c664272b1639f820357e7195b1415937f
HanRachid <handaouirachid@gmail.com>
2024-01-02 13:24:33 +0100

    remove trailing whitespace

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -25,7 +25,6 @@ if len(sys.argv) == 2:
         if task["completed"]:
             NUMBER_OF_DONE_TASKS += 1
             DONE_TASKS += "\t " + task["title"] + "\n"
-    DONE_TASKS = DONE_TASKS.rstrip("\n")
     output = "Employee {} is done with tasks({}/{}):\n {}".format(
         EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS, DONE_TASKS
     )
