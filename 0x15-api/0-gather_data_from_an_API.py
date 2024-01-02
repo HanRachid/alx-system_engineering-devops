eb880569931e1a574c8a514976ef1f2dfc962eff
HanRachid <handaouirachid@gmail.com>
2024-01-02 12:47:58 +0100

    start 0x15 apis

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+
+import requests
+
+if (len(sys.argv) == 2):
+
+    ruser = requests.get(
+        'https://jsonplaceholder.typicode.com/users/'+sys.argv[1])
+    userresponse = ruser.json()
+    rtodo = requests.get(
+        'https://jsonplaceholder.typicode.com/users/'+sys.argv[1]+'/todos')
+    EMPLOYEE_NAME = userresponse['name']
+    todoresponse = rtodo.json()
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+    DONE_TASKS = ''
+    for task in todoresponse:
+        TOTAL_NUMBER_OF_TASKS += 1
+        if task['completed'] == True:
+            NUMBER_OF_DONE_TASKS += 1
+            DONE_TASKS += '\t '+task['title']+'\n'
+    print(
+        f'Employee {EMPLOYEE_NAME} is done with tasks ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
+    print(DONE_TASKS)
---
+++ 0x15-api/README.md
@@ -0,0 +1,1 @@
+working with some apis on python using the requests library
