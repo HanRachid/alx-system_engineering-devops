5a5d8d59cdb6aadcc78fd0f7c060ff30513301e1
RachidHnd <rachid.handaoui@axa-assistance.ma>
2024-08-13 21:30:26 +0100

    add files

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """
-Returns the number of subscribers in a subreddit
+Contains the number_of_subscribers function
 """
+
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """returns the numbers of subscribers in a given subreddit"""
+    """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit), headers={'User-Agent': '0x16-api_advanced:project'}).json()
-    subs = r.get("data",{}).get("subscribers",0)
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
     return subs
