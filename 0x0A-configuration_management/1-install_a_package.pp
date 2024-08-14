926c7e63e0fde440935e0b15c3ed21bb9ac332fe
RachidHnd <rachid.handaoui@axa-assistance.ma>
2024-08-14 20:10:01 +0100

    fix version

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,8 @@
 #Install flask version 2.1.0 package
 package { 'Werkzeug':
-  ensure   => '2.1.1',
+  ensure   => '0.16.1',
   provider => 'pip3',
-},
+}
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
