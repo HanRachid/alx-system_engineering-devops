6ce5a6f83c83d64f9b714fdb1686f7a68254ec24
younessmekkaoui <mekkayouness@gmail.com>
2024-08-19 14:50:07 +0100

    After aadding the test scripts

--- execute_command.c
+++ execute_command.c
@@ -1,58 +1,28 @@
 #include "shell.h"
-
-void execute_command(char *command)
+int exeCommandLine(char *argv[MAX_ARGS])
 {
-    char *argv[MAX_ARGS];
-    char *path, *dir, *cmd_path;
-    char *path_env = getenv("PATH");
-    int i = 0;
-    pid_t pid;
-
-    argv[i] = strtok(command, " ");
-    while (argv[i] != NULL && i < MAX_ARGS - 1)
-        argv[++i] = strtok(NULL, " ");
-
-    if (argv[0][0] == '/' || argv[0][0] == '.')
-    {
-        if (access(argv[0], X_OK) == 0)
-        {
-            pid = fork();
-            if (pid == 0)
-                execve(argv[0], argv, NULL);
-            else
-                wait(NULL);
-        }
-        else
-        {
-            perror(argv[0]);
-        }
-        return;
-    }
-
-    path = strdup(path_env);
-    dir = strtok(path, ":");
-    while (dir != NULL)
-    {
-        cmd_path = malloc(strlen(dir) + strlen(argv[0]) + 2);
-        sprintf(cmd_path, "%s/%s", dir, argv[0]);
-
-        if (access(cmd_path, X_OK) == 0)
-        {
-            pid = fork();
-            if (pid == 0)
-                execve(cmd_path, argv, NULL);
-            else
-                wait(NULL);
-            
-            free(cmd_path);
-            free(path);
-            return;
-        }
-
-        free(cmd_path);
-        dir = strtok(NULL, ":");
-    }
-
-    fprintf(stderr, "%s: command not found\n", argv[0]);
-    free(path);
+	int fflag = 0;
+	/* ------------------fork to child execve-------------------------*/
+	fflag = fork();
+	if (fflag == -1)
+	{
+i		/*printf("fealuire");*/
+		return (-1);
+	}
+	else if (fflag == 0)
+	{
+		/* --------------------------child execve--------------------*/
+		if (execve(argv[0], (char **)argv, NULL) == -1)
+		{
+			/*printf("No such file or directory\n");*/
+			return (-1);
+		}
+	}
+	else
+	{
+		/* --------------------------wait child execve--------------------*/
+		wait(NULL);
+		return (0);
+	}
+	return (2);
 }
