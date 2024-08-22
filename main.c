8bf0b7908c86706585569ac7a25b610c15f2b2b7
younessmekkaoui <mekkayouness@gmail.com>
2024-08-22 11:31:00 +0100

    after betty remod

--- main.c
+++ main.c
@@ -1,52 +1,156 @@
-#include "shell.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "main.h"
 
-int main(void)
+/**
+ * execute - executes command passed as parameter
+ * @command: command passed to execute
+ * @av: first arg from argv
+ * @env: environment from main
+ * @count: command count
+ * Return: void;
+ */
+
+void execute(char *command, char *av, char **env, int count)
 {
-    char *buffer = NULL;
-    size_t bufsize = 0;
-    char *argv[MAX_ARGS];
+	char **argv;
 
-    while (1)
-    {
-        printf("shell$ "); // Display prompt
-        if (getline(&buffer, &bufsize, stdin) == -1)
-        {
-            perror("getline error");
-            free(buffer);
-            exit(EXIT_FAILURE);
-        }
+	argv = strtokarray(command, " \n\r\t");
+	argv[0] = strtok(argv[0], " \n\r\t");
 
-        // Parse input into argv array
-        int i = 0;
-        char *token = strtok(buffer, " \n");
-        while (token != NULL && i < MAX_ARGS - 1)
-        {
-            argv[i++] = token;
-            token = strtok(NULL, " \n");
-        }
-        argv[i] = NULL;
+	if (strcmp(argv[0], "(null)") != 0)
+	{
+		if (strcmp(argv[0], "env") == 0)
+		{
+			if (execve("/bin/env", argv, env) == -1)
+			{
+				printf("%s: %d: %s: not found\n", av, count, argv[0]);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			if (execve(argv[0], argv, env) == -1)
+			{
+				printf("%s: %d: %s: not found\n", av, count, argv[0]);
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
+/**
+ * shell_interactive - starts shell
+ * @av: first arg from argv
+ * @env: environment from main
+ * Return: void;
+ */
 
-        if (argv[0] == NULL) // Check if no command was entered
-            continue;
+void shell_interactive(char *av, char **env)
+{
+	char *line = "";
+	size_t size = 0;
+	pid_t child;
+	int count = 0;
+	char cwd[1024];
+	int checkpwd, checkexit, checknexit, status;
 
-        if (_strcmp(argv[0], "exit") == 0) // Exit command
-        {
-            free(buffer);
-            exit(0);
-        }
+	printf("($) ");
+	while (getline(&line, &size, stdin) != -1)
+	{
 
-        if (_strcmp(argv[0], "env") == 0) // Env command
-        {
-            print_env();
-            continue;
-        }
+		checkexit = strcmp(line, "exit\n");
+		checknexit = strncmp(line, "exit ", 5);
+		checkpwd = strcmp(line, "pwd\n");
+		count++;
+		child = fork();
+		if (checkexit == 0 || checknexit == 0)
+			break;
+		if (child == 0 && strcmp(line, "pwd\n") == 0)
+		{
+			if (getcwd(cwd, sizeof(cwd)) != NULL)
+				printf("%s\n", cwd);
+			else
+				perror("getcwd() error");
+		}
 
-        if (exeCommandLine(argv) == -1)
-        {
-            perror("Execution failed");
-        }
-    }
+		if (child == 0 && checkpwd != 0 && checkexit != 0)
+		{
+			execute(line, av, env, count);
+			break;
+		}
+		else
+		{
+			wait(&status);
+			printf("($) ");
+		}
+	}
+	exit(0);
+}
 
-    free(buffer);
-    return 0;
+/**
+ * shell_nonint - executes command in non interactive mode
+ * @av: first arg from argv
+ * @env: array of chars containing env
+ * Return: void;
+ */
+
+void shell_nonint(char *av, char **env)
+{
+	char *line = NULL;
+	size_t size = 0;
+	pid_t child;
+	int count = 0;
+	char cwd[1024];
+	int checkpwd, checkexit, checknexit, status;
+
+	while (getline(&line, &size, stdin) != -1)
+	{
+		checknexit = strncmp(line, "exit ", 5);
+		checkexit = strcmp(line, "exit\n");
+		checkpwd = strcmp(line, "pwd\n");
+		count++;
+		child = fork();
+		if (checkexit == 0 || checknexit == 0)
+			break;
+		if (child == 0 && strcmp(line, "pwd\n") == 0)
+		{
+			if (getcwd(cwd, sizeof(cwd)) != NULL)
+				printf("%s\n", cwd);
+			else
+				perror("getcwd() error");
+		}
+
+		if (child == 0 && checkpwd != 0 && checkexit != 0)
+		{
+			execute(line, av, env, count);
+			break;
+		}
+		else
+			wait(&status);
+	}
+	free(line);
+	exit(0);
+}
+
+/**
+ * main - starts shell
+ * @ac: arg count
+ * @av: arg array
+ * @env: environment
+ * Return: void;
+ */
+
+int main(int ac, char **av, char **env)
+{
+	(void)ac;
+	if (isatty(0))
+		shell_interactive(av[0], env);
+	else
+		shell_nonint(av[0], env);
+	return (0);
 }
